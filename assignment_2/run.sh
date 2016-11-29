05f1e4f7b09de0dc5f0bb728b4a883748caaea3c
cheng wei <michael1996623@gmail.com>
2016-11-29 10:50:22 -0800

    minor fix

--- assignment_2/run.sh
+++ assignment_2/run.sh
@@ -2,11 +2,19 @@
 
 make clean
 make
+
+tmp="...................."
+
+echo "$tmp"
 echo "mode 0"
 ./agent 10 0
+echo "$tmp"
 
 echo "mode 1"
 ./agent 10 1
+echo $tmp
 
-echo "mode 2"
+echo "mode 2 (bonus)"
 ./agent 10 2
+echo $tmp
+
